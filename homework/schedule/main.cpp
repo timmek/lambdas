0b8e879eae8bbec8e1591d5835dfc61738eeda3c
Andrzej Rafalski <anraf1001@gmail.com>
2022-06-20 16:28:17 +0200

    format task files according to clang-format file

--- homework/schedule/main.cpp
+++ homework/schedule/main.cpp
@@ -18,9 +18,9 @@ int main() {
     using namespace std::chrono_literals;
 
     auto start = std::chrono::system_clock::now();
-    schedule([]{ std::cout << "I'm an empty function\n"; }, 1s);
-    schedule([](int a){ std::cout << "Param int a = " << a << '\n'; }, 2s, 42);
-    schedule([](std::string s, double d){ std::cout << "Params: string s = " << s << ", double d = " << d << '\n'; }, 0s, "text", 42.5);
+    schedule([] { std::cout << "I'm an empty function\n"; }, 1s);
+    schedule([](int a) { std::cout << "Param int a = " << a << '\n'; }, 2s, 42);
+    schedule([](std::string s, double d) { std::cout << "Params: string s = " << s << ", double d = " << d << '\n'; }, 0s, "text", 42.5);
     auto stop = std::chrono::system_clock::now();
 
     std::chrono::duration<double> diff = stop - start;
