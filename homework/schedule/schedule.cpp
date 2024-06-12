36f171e573842c80e3f9de7f6b7fec9b6ca1e90d
timmek <timm.michal@gmail.com>
2024-06-12 07:39:52 +0200

    Create schedule.cpp

---
+++ homework/schedule/schedule.cpp
@@ -0,0 +1,26 @@
+void schedule( std::function<void()> func, std::chrono::seconds sec)
+{
+    auto start = std::chrono::system_clock::now();
+    while ((std::chrono::system_clock::now() - start) < (std::chrono::duration < double>)sec)
+        continue;
+
+    func();
+}
+
+void schedule(std::function<void(int)> func, std::chrono::seconds sec, int number)
+{
+    auto start = std::chrono::system_clock::now();
+    while ((std::chrono::system_clock::now() - start) < (std::chrono::duration < double>)sec)
+        continue;
+
+    func(number);
+}
+
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds sec, std::string text, double number)
+{
+    auto start = std::chrono::system_clock::now();
+    while ((std::chrono::system_clock::now() - start) < (std::chrono::duration < double>)sec)
+        continue;
+
+    func(text, number);
+}
